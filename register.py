7d0f196808475833d3513728b6db6308d85e4571
berlianrezki <onedheone@gmail.com>
2022-07-27 19:19:03 +0700

    GoneTone01

--- register.py
+++ register.py
@@ -16,13 +16,13 @@ class TestRegist(unittest.TestCase):
         time.sleep(3)
         browser.find_element(By.ID,"signUp").click()
         time.sleep(1)
-        browser.find_element(By.ID, "name_register").send_keys("GoneTone03")
+        browser.find_element(By.ID, "name_register").send_keys("GoneTone06")
         time.sleep(1)
-        browser.find_element(By.ID, "email_register").send_keys("GoneTone03@gmail.com")
+        browser.find_element(By.ID, "email_register").send_keys("GoneTone06@gmail.com")
         time.sleep(1)
         browser.find_element(By.ID, "password_register").send_keys("123456")
         time.sleep(1)
-        browser.find_element(By.ID,"signup_register").click() # klik tombol sign in
+        browser.find_element(By.ID,"signup_register").click() # klik tombol sign up
         time.sleep(1)
 
         # validasi
@@ -45,7 +45,7 @@ class TestRegist(unittest.TestCase):
         time.sleep(1)
         browser.find_element(By.ID, "password_register").send_keys("123456")
         time.sleep(1)
-        browser.find_element(By.ID,"signup_register").click() # klik tombol sign in
+        browser.find_element(By.ID,"signup_register").click() # klik tombol sign up
         time.sleep(1)
 
         # validasi
@@ -53,6 +53,29 @@ class TestRegist(unittest.TestCase):
         response_message = browser.find_element(By.ID,"swal2-content").text
 
         self.assertIn('Email sudah terdaftar, gunakan Email lain', response_data)
+        self.assertEqual(response_message, 'Gagal Registrasi')
+
+    def test_a_failed_register_without(self): 
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("http://barru.pythonanywhere.com/daftar") # buka situs
+        time.sleep(3)
+        browser.find_element(By.ID,"signUp").click()
+        time.sleep(1)
+        browser.find_element(By.ID, "name_register").send_keys("GoneTone05") # isi nama
+        time.sleep(1)
+        browser.find_element(By.ID, "email_register").send_keys("GoneTone05@gmail.com")
+        time.sleep(1)
+        browser.find_element(By.ID, "password_register").send_keys("")
+        time.sleep(1)
+        browser.find_element(By.ID,"signup_register").click() # klik tombol sign in
+        time.sleep(1)
+
+        # validasi
+        response_data = browser.find_element(By.ID,"swal2-title").text
+        response_message = browser.find_element(By.ID,"swal2-content").text
+
+        self.assertIn('Email/Username/Password tidak boleh kosong', response_data)
         self.assertEqual(response_message, 'Gagal Registrasi')
 
     def tearDown(self): 
